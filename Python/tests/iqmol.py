8b86a4ba1233f2059781e5618a6f8ec40e109b55
joshkamm <joshkamm@umich.edu>
2019-08-19 14:58:30 -0400

    switched to reading molfile instead of xyz for rdkit compatibility

--- Python/tests/iqmol.py
+++ Python/tests/iqmol.py
@@ -1,6 +1,8 @@
 import pybel as pb
 from pathlib import Path
-import rdkit
+from rdkit.Chem.rdmolfiles import MolFromSmiles, MolToMolBlock, MolFromMolFile,\
+    MolToSmiles
+from rdkit.Chem.rdForceFieldHelpers import UFFOptimizeMoleculeConfs
 
 class IQmol:
     def __init__(self):
@@ -10,13 +12,17 @@ class IQmol:
         self.molecules = {}
         self.optimized_molecules = {}
         for file in self.iqmol_scratch_path.iterdir():
-            print(next(pb.readfile('xyz', str(file))))
-            self.molecules[file.stem] = next(pb.readfile('xyz', str(file)))
-            print(self.molecules[file.stem].write('xyz'))
-            self.optimized_molecules[file.stem] = next(pb.readfile('xyz', str(file)))
-            self.optimized_molecules[file.stem].localopt()
-            print(self.optimized_molecules[file.stem].write('xyz'))
+            print(MolToSmiles(MolFromMolFile(str(file))))
+            self.molecules[file.stem] = MolFromMolFile(str(file))
+            mol = MolFromMolFile(str(file))
+            print(MolToMolBlock(mol))
+            UFFOptimizeMoleculeConfs(mol)
+            print(MolToMolBlock(mol))
+            self.optimized_molecules[file.stem] = mol
+            print(MolToSmiles(self.optimized_molecules[file.stem]))
 
 if __name__ == '__main__':
     print('Test Conformer ML version 0.0.0')
     IQmol().update()
+#     mol = MolFromSmiles('CCC')
+#     print(MolToMolBlock(mol))
