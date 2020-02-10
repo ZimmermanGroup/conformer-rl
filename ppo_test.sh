7641e3d061dac54b9cc18000d0a69e62df19dc22
Runxuan Jiang <runxuan.jiang@gmail.com>
2020-02-10 17:26:08 -0500

    try running gpu

---
+++ ppo_test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# The interpreter used to execute the script
+
+
+#“#SBATCH” directives that convey submission options:
+
+#SBATCH --job-name=eval
+#SBATCH --mail-user=runxuanj@umich.edu
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=2048m
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --time=10:00:00
+#SBATCH --account=tewaria1
+#SBATCH --partition=gpu
+#SBATCH --output=/home/%u/%x-%j.log
+#SBATCH --get-user-env
+# The application(s) to execute along with its input arguments and options:
+source deactivate my-rdkit-env
+source ~/.bashrc
+cd ~/conformer-ml/
+module load cuda
+module load gcc
+python drl-a2c.py
