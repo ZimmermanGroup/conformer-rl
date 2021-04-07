476d92896dc006fef0f481c4647604d54c58b21f
joshkamm <joshkamm@umich.edu>
2021-04-07 10:11:21 -0400

    added a few doctests for #30

--- generate_molecule/alkyl_phenyl.py
+++ generate_molecule/alkyl_phenyl.py
@@ -13,9 +13,25 @@ from stko.molecular.torsions.torsion import Torsion
 
 
 class AlkylPhenylPolymer:
-    """a polymer of alkyl chains bridged with phenyl groups"""
+    """a polymer of alkyl chains bridged with phenyl groups
     
-    def __init__(self, num_repeating_units=3):
+    This is a preliminary implementation with a fixed pattern of building blocks due to simplicity
+    of cheminformatic manipulation.
+    
+    >>> ap_polymer = AlkylPhenylPolymer(num_repeating_units=2)
+    
+    test on the first torsion in the second building block
+    >>> test_torsion_info = ap_polymer.polymer.get_torsion_infos_by_building_block()[1][0]
+    >>> test_torsion_info.torsion
+    Torsion(atom1=C(16), atom2=C(22), atom3=C(23), atom4=C(24))
+    >>> test_torsion_info.building_block_torsion
+    Torsion(atom1=C(0), atom2=C(12), atom3=C(13), atom4=C(14))
+    
+    >>> mol = ap_polymer.polymer.stk_molecule.to_rdkit_mol()
+    >>> display(Draw.MolToImage(mol_with_atom_index(mol), size=(700, 300)))
+    """
+    
+    def __init__(self, num_repeating_units=2):
         lengths = [7, 12]
         repeating_unit = 'AB'
         
@@ -40,28 +56,31 @@ class AlkylPhenylPolymer:
             )
         )
         
-        # fix up the bonds
+        # clean up the aromatic double bonds
         boundary_bonds = [bond_info.get_bond() for bond_info in polymer.get_bond_infos()
                         if bond_info.get_building_block_id() is None]
         for bond in islice(boundary_bonds, 1, len(boundary_bonds), 2):
             bond._order = 2
-        
-        display(Draw.MolToImage(mol_with_atom_index(polymer.to_rdkit_mol()),
-                                size=(700, 300)))
         
         self.polymer = ConstructedMoleculeTorsioned(polymer)
         
     
     @staticmethod
     def make_monomer(length):
+        """make an alkyl chain with two phenyl end caps
+        
+        >>> length = 7
+        >>> ap_monomer = AlkylPhenylPolymer.make_monomer(length)
+        >>> ap_monomer.get_num_atoms() == 6 * 2 + length
+        True
+        
+        >>> display(Draw.MolToImage(mol_with_atom_index(ap_monomer.to_rdkit_mol()),
+        ...                        size=(700, 300)))
+        """
         alkane = init_building_block(smiles='F' + 'C' * length + 'F',
                                      functional_groups=[stk.FluoroFactory()])
         benzene = init_building_block(
             smiles='FC1=CC=CC=C1', functional_groups=[stk.FluoroFactory()])
-        # display(Draw.MolToImage(mol_with_atom_index(alkane.to_rdkit_mol()),
-        #                         size=(700, 300)))
-        # display(Draw.MolToImage(mol_with_atom_index(benzene.to_rdkit_mol()),
-        #                         size=(700, 300)))
 
         alkyl_phenyl = stk.ConstructedMolecule(
             topology_graph=stk.polymer.Linear(
@@ -71,20 +90,13 @@ class AlkylPhenylPolymer:
                 optimizer=stk.MCHammer(),
             )
         )
-        display(Draw.MolToImage(mol_with_atom_index(alkyl_phenyl.to_rdkit_mol()),
-                                size=(700, 300)))
 
         return stk.BuildingBlock.init_from_molecule(alkyl_phenyl)
 
 
 if __name__ == '__main__':
-    polymer = AlkylPhenylPolymer()
-    polymer.polymer.stk_molecule.to_rdkit_mol()
-    # mol = init_building_block(smiles='F' + 'C' * length + 'F',
-    #                                  functional_groups=[stk.FluoroFactory()]).to_rdkit_mol()
-    # nglview.show_rdkit(mol)
+    import doctest
+    doctest.testmod(optionflags=doctest.NORMALIZE_WHITESPACE |
+                    doctest.ELLIPSIS, verbose=True)
 
-    for test_torsion_info in polymer.polymer.get_torsion_infos_by_building_block()[2]:
-        print(test_torsion_info.torsion)
-        print(test_torsion_info.building_block_torsion)
 # %%
