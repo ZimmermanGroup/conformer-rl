df0bcced4e064b9f6a6263d3a7a55fa6ed3410de
joshkamm <joshkamm@umich.edu>
2021-04-28 03:54:11 -0400

    Experimenting with pytest for #34

--- generate_molecule/test_xor_gate.py
+++ generate_molecule/test_xor_gate.py
@@ -1,4 +1,13 @@
+from generate_molecule.xor_gate import XorGate
+
 import pytest
 
 def test_xor_gate():
-    assert True
+    xor_gate = XorGate(gate_complexity=2, num_gates=3)
+    torsion_list = xor_gate.polymer.get_torsion_list()
+    assert torsion_list == [[1, 0, 7, 6],
+                            [10, 9, 14, 13],
+                            [17, 16, 21, 20],
+                            [24, 23, 28, 27],
+                            [32, 33, 35, 34],
+                            [39, 40, 42, 41]]
