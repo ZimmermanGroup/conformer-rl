1a4466b292d003c1eeda5cb49d9219dc6e1bd454
joshkamm <joshkamm@umich.edu>
2021-04-30 16:32:06 -0400

    Finished xor_gate pytest drafting for #34

--- generate_molecule/test_xor_gate.py
+++ generate_molecule/test_xor_gate.py
@@ -2,8 +2,6 @@ from generate_molecule.xor_gate import XorGate
 import stk
 from stko.molecular.torsions.torsion import Torsion
 
-import pytest
-
 def test_xor_gate():
     xor_gate = XorGate(gate_complexity=2, num_gates=3)
     torsion_list = xor_gate.polymer.get_torsion_list()
@@ -14,7 +12,8 @@ def test_xor_gate():
                             [32, 33, 35, 34],
                             [39, 40, 42, 41]]
     test_torsion_info, *rest = xor_gate.polymer.get_torsion_infos_by_building_block()[2]
-    assert str(test_torsion_info.torsion) == str(Torsion(atom1=stk.C(32), atom2=stk.C(33), atom3=stk.C(35), atom4=stk.C(34)))
-
-def test_xor_gate_torsion_info():
-    pass
+    assert str(test_torsion_info.torsion) == str(Torsion(atom1=stk.C(32), atom2=stk.C(33),
+                                                         atom3=stk.C(35), atom4=stk.C(34)))
+    assert str(test_torsion_info.building_block_torsion) == str(Torsion(atom1=stk.C(2), atom2=stk.C(3),
+                                                                        atom3=stk.C(7), atom4=stk.C(6)))
+    assert xor_gate.num_torsions == xor_gate.num_gates * xor_gate.gate_complexity
