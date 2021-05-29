e385d237c01514e44a279f79283bb62b960f2ad1
Runxuan Jiang <runxuan.jiang@gmail.com>
2021-05-29 11:34:05 -0400

    updated anlaysis module

--- src/torsionnet/analysis/analysis.py
+++ src/torsionnet/analysis/analysis.py
@@ -6,6 +6,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from rdkit import Chem
 from rdkit.Chem import AllChem, TorsionFingerprints
+from torsionnet.utils import tfd_matrix
 
 def _load_from_pickle(filename):
     infile = open(filename, 'rb')
@@ -28,15 +29,9 @@ def load_data_from_pickle(paths, indices=None):
         for key, val in datum.items():
             if isinstance(val, dict):
                 for subkey, subval in val.items():
-                    if not subkey in final_data:
-                        final_data[subkey] = [subval]
-                    else:
-                        final_data[subkey].append(subval)
+                    final_data.setdefault(subkey, []).append(subval)
             else:
-                if not key in final_data:
-                    final_data[key] = [val]
-                else:
-                    final_data[key].append(val)
+                final_data.setdefault(key, []).append(val)
     
     return final_data
 
@@ -48,32 +43,38 @@ def bar_plot_episodic(key, data):
 def histogram_episodic(key, data):
     n = len(data["indices"])
     fig, axes = plt.subplots(ncols=n, figsize=(20, 8))
-    for i, index in enumerate(data["indices"]):
-        sns.histplot(data=data[key][i], stat="probability", ax=axes[i])
-        axes[i].set(xlabel=index)
+
+    if n == 1:
+        sns.histplot(data=data[key][0], stat="probability", ax=axes)
+        axes.set(xlabel=data['indices'][0])
+    else:
+        for i, index in enumerate(data["indices"]):
+            sns.histplot(data=data[key][i], stat="probability", ax=axes[i])
+            axes[i].set(xlabel=index)
 
     return fig, axes
 
-def tfd_matrix(mols):
-    dists = np.zeros((len(mols), len(mols)), dtype=float)
-    for i in range(len(mols)):
-        for j in range(i, len(mols)):
-            dists[i][j] = TorsionFingerprints.GetTFDBetweenMolecules(mols[i], mols[j], 0, 0)
-            dists[j][i] = dists[i][j]
-    return dists
+def heatmap_episodic(key, data):
+    pass
 
-def diversity_heatmap(data):
+def tfd_heatmap(data):
+    if not 'mol' in data:
+        raise Exception('data dict must contain RDKit Mol object with \'mol\' key to generate tfd heatmap.')
     n = len(data["indices"])
     fig, axes = plt.subplots(ncols=n, figsize=(20, 8))
-    for i, index in enumerate(data["indices"]):
-        sns.heatmap(data=tfd_matrix(data['molecule'][i]), ax=axes[i])
-        axes[i].set(xlabel=index)
+    if n == 1:
+        sns.heatmap(data=tfd_matrix(data['mol'][0]), ax=axes)
+        axes.set(xlabel=data['indices'][0])
+    else:
+        for i, index in enumerate(data["indices"]):
+            sns.heatmap(data=tfd_matrix(data['mol'][i]), ax=axes[i])
+            axes[i].set(xlabel=index)
     
     return fig, axes
 
 def diversity_bar(data):
     diversity = []
-    for molecules in data['molecule']:
+    for molecules in data['mol']:
         diversity.append(np.sum(tfd_matrix(molecules)))
     ax = sns.barplot(x=data["indices"], y=diversity)
     ax.set(xlabel='run', ylabel='diversity metric')
