08165b9e4cd77270d375c7ff9579fe9a565346db
joshkamm <joshkamm@umich.edu>
2021-08-02 17:47:49 -0400

    wip #49

--- src/conformer_rl/analysis/josh_lignin_analysis.py
+++ src/conformer_rl/analysis/josh_lignin_analysis.py
@@ -7,19 +7,48 @@ from rdkit import Chem
 import pandas as pd
 import altair as alt
 
-pickle_path = Path('/export/zimmerman/joshkamm/ConformerML/conformer-ml/src/conformer_rl/analysis/pruned_mol_0.1tfd.pkl')
-with open(pickle_path, 'rb') as pickle_file:
-    md_mol = pickle.load(pickle_file)
-mol_path = pickle_path.parent / 'test.mol'
-Chem.rdmolfiles.MolToMolFile(
-    md_mol, str(mol_path), confId=6747)
-num_conformers = md_mol.GetNumConformers()
-confs = list(md_mol.GetConformers())
-distances = [conf.GetAtomPosition(100).Distance(conf.GetAtomPosition(226))
-             for conf in confs]
-df = pd.DataFrame({'distances' : distances})
+# pickle_path = Path('/export/zimmerman/joshkamm/ConformerML/conformer-ml/src/conformer_rl/analysis/pruned_mol_0.1tfd.pkl')
+pickle_paths = {}
+RL, MD = 'RL', 'MD'
+pickle_paths[RL] = Path('/export/zimmerman/joshkamm/from_epunzal/neurips/pruned_files_ecutoff500/rl_mol_ecutoff.pkl') # RL conformers
+pickle_paths[MD] = Path('/export/zimmerman/joshkamm/from_epunzal/neurips/pruned_files_ecutoff500/sgld_mol_ecutoff.pkl') # MD conformers
+
+rdkit_mols = {}
+for data_source, pickle_path in pickle_paths.items():
+    with open(pickle_path, 'rb') as pickle_file:
+        rdkit_mols[data_source] = pickle.load(pickle_file)
+
+
+
+# mol_path = pickle_path.parent / 'test.mol'
+# Chem.rdmolfiles.MolToMolFile(
+#     md_mol, str(mol_path), confId=6747)
+num_conformers = {data_source: rdkit_mol.GetNumConformers() for data_source, rdkit_mol in rdkit_mols.items()}
+df = pd.DataFrame.from_records([(data_source, 1 / num_conformers[data_source],
+                                 conf.GetAtomPosition(100).Distance(conf.GetAtomPosition(226)))
+                                for data_source in rdkit_mols
+                                for conf in rdkit_mols[data_source].GetConformers()],
+                               columns=['data_source', 'weight', 'distance'])
+
+
+# distances = [conf.GetAtomPosition(100).Distance(conf.GetAtomPosition(226))
+#              for conf in confs]
+alt.data_transformers.disable_max_rows()
 alt.Chart(df).mark_bar().encode(
-    alt.X('distances:Q', bin=True),
-    y='count()'
-)
+    alt.X('distance:Q', bin=alt.Bin(maxbins=30)),
+    y=alt.Y('sum(weight)', axis=alt.Axis(format='%', title=None)),
+    row='data_source:N'
+).properties(
+    width=200,
+    height=200
+)#.save('chart.png', scale_factor=3.0)
+# alt.Chart(df).transform_joinaggregate(
+#     total='count(*)'
+# ).transform_calculate(
+#     pct='1 / datum.total'
+# ).mark_bar().encode(
+#     alt.X('distance:Q', bin=True),
+#     alt.Y('sum(pct):Q', axis=alt.Axis(format='%')),
+#     row='data_source:N'
+# )
 
