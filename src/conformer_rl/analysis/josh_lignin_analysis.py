c5631820b999c99e26d0b0fa8ebe2f3011677637
joshkamm <joshkamm@umich.edu>
2022-01-05 23:15:52 -0500

    wip #49 restructuring for multiple mechanisms

--- src/conformer_rl/analysis/josh_lignin_analysis.py
+++ src/conformer_rl/analysis/josh_lignin_analysis.py
@@ -14,6 +14,7 @@ from conformer_rl.analysis.lignin_pericyclic import \
     LigninPericyclicCalculator, LigninPericyclicFunctionalGroupFactory, \
     LigninMaccollCalculator, LigninMaccollFunctionalGroupFactory
 
+import param
 import hvplot.pandas # noqa
 import holoviews as hv
 from holoviews import opts
@@ -25,54 +26,66 @@ from panel_chemistry.pane.ngl_viewer import EXTENSIONS
 pn.extension('bokeh', comms='vscode')
 pn.extension("ngl_viewer", sizing_mode="stretch_width")
 
-# %%
-mol = setup_mol()
-mol = Chem.rdmolops.AddHs(mol)
-Chem.rdmolops.Kekulize(mol)
-stk_mol = stk.BuildingBlock.init_from_rdkit_mol(
-    mol,
-    functional_groups=(
-        LigninMaccollFunctionalGroupFactory(),
-    )
-)
-maccoll_distances = LigninMaccollCalculator().calculate_distances(mol)
+## %%
+class LigninDashboard(param.Parameterized):
+    mechanism = param.Selector(['Pericylic', 'Maccoll'])
 
-def highlighted_mol(func_group_id):
-    mol = setup_mol()
-    for i, f_group in enumerate(stk_mol.get_functional_groups()):
-        atom_H = mol.GetAtomWithIdx(f_group.H.get_id())
-        atom_O = mol.GetAtomWithIdx(f_group.O.get_id())
-        if i == int(func_group_id):
-            atom_H.SetAtomicNum(9)
-            atom_O.SetAtomicNum(15)
-        else: # gently highlight other functional groups in the same conformer
-            atom_H.SetAtomicNum(2)
-    return mol
+    def __init__(self):
+        self.mol = setup_mol()
+        maccoll_distances = LigninMaccollCalculator().calculate_distances(self.mol)
+        
+        self.df = maccoll_distances.to_dataframe().reset_index().astype({FUNC_GROUP_ID_1: 'str'})
+        self.stk_mol = self.setup_stk_mol()
+    
+    def scatter_plot(self):
+        points = self.df.hvplot.scatter(x='Lignin Maccoll mechanism distances', y='Energies', c='func_group_id_1')
+        points.opts(
+            opts.Scatter(tools=['tap', 'hover'], active_tools=['wheel_zoom'], width=600, height=600,
+                        marker='triangle', size=10,)
+        )
+        self.stream = Selection1D(source=points)
+        return points
+    
+    def app(self):
+        return pn.Row(pn.Column(self.param, points, index_conf), display_mol)
+    
+    def setup_stk_mol(self):
+        self.mol = Chem.rdmolops.AddHs(self.mol)
+        Chem.rdmolops.Kekulize(self.mol)
+        return stk.BuildingBlock.init_from_rdkit_mol(
+            self.mol,
+            functional_groups=(
+                LigninMaccollFunctionalGroupFactory(),
+            )
+        )
+        
+    @pn.depends(stream.param.index)
+    def display_mol(self, index):
+        if not index:
+            return None
+        conf_id = int(self.df.iloc[index]['conf_id'])
+        pdb_block = MolToPDBBlock(self.highlighted_mol(self.df.iloc[index][FUNC_GROUP_ID_1]), confId=conf_id)
+        viewer = NGLViewer(object=pdb_block, extension='pdb', background="#F7F7F7", min_height=800, sizing_mode="stretch_both")
+        return viewer
 
-df = maccoll_distances.to_dataframe().reset_index().astype({FUNC_GROUP_ID_1: 'str'})
-print(df)
-points = df.hvplot.scatter(x='Lignin Maccoll mechanism distances', y='Energies', c='func_group_id_1')
-points.opts(
-    opts.Scatter(tools=['tap', 'hover'], active_tools=['wheel_zoom'], width=600, height=600,
-                 marker='triangle', size=10,)
-)
 
-stream = Selection1D(source=points)
-@pn.depends(stream.param.index)
-def display_mol(index):
-    if not index:
-        return None
-    conf_id = int(df.iloc[index]['conf_id'])
-    pdb_block = MolToPDBBlock(highlighted_mol(df.iloc[index][FUNC_GROUP_ID_1]), confId=conf_id)
-    viewer = NGLViewer(object=pdb_block, extension='pdb', background="#F7F7F7", min_height=800, sizing_mode="stretch_both")
-    return viewer
+    @pn.depends(stream.param.index)
+    def index_conf(self, index):
+        return index
 
+    def highlighted_mol(self, func_group_id):
+        mol = setup_mol()
+        for i, f_group in enumerate(self.stk_mol.get_functional_groups()):
+            atom_H = mol.GetAtomWithIdx(f_group.H.get_id())
+            atom_O = mol.GetAtomWithIdx(f_group.O.get_id())
+            if i == int(func_group_id):
+                atom_H.SetAtomicNum(9)
+                atom_O.SetAtomicNum(15)
+            else: # gently highlight other functional groups in the same conformer
+                atom_H.SetAtomicNum(2)
+        return mol
 
-@pn.depends(stream.param.index)
-def index_conf(index):
-    return index
-app = pn.Row(pn.Column(points, index_conf), display_mol)
-app.show()
+LigninDashboard().show()
 
 # %%
 
